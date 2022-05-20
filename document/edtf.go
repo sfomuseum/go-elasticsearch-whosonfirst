3bde98a9e24f986e6fa5f0d65f17cfa710a5c34e
thisisaaronland <aaron@localhost>
2022-05-20 10:01:29 -0700

    update vendor deps

--- document/edtf.go
+++ document/edtf.go
@@ -3,22 +3,11 @@ package document
 import (
 	"context"
 	"fmt"
-	"github.com/sfomuseum/go-edtf"
-	"github.com/sfomuseum/go-edtf/parser"
+	"github.com/sfomuseum/go-edtf/unix"
 	"github.com/tidwall/gjson"
 	"github.com/tidwall/sjson"
 	_ "log"
 )
-
-type date_span struct {
-	start int64
-	end   int64
-}
-
-type date_range struct {
-	outer *date_span
-	inner *date_span
-}
 
 // AppendEDTFRanges appends numeric date ranges derived from `edtf:inception` and `edtf:cessation` properties
 // to a Who's On First document.
@@ -47,17 +36,17 @@ func AppendEDTFRanges(ctx context.Context, body []byte) ([]byte, error) {
 	to_assign := make(map[string]int64)
 
 	if inception_range != nil {
-		to_assign["date:inception_inner_start"] = inception_range.inner.start
-		to_assign["date:inception_inner_end"] = inception_range.inner.end
-		to_assign["date:inception_outer_start"] = inception_range.outer.start
-		to_assign["date:inception_outer_end"] = inception_range.outer.end
+		to_assign["date:inception_inner_start"] = inception_range.Inner.Start
+		to_assign["date:inception_inner_end"] = inception_range.Inner.End
+		to_assign["date:inception_outer_start"] = inception_range.Outer.Start
+		to_assign["date:inception_outer_end"] = inception_range.Outer.End
 	}
 
 	if cessation_range != nil {
-		to_assign["date:cessation_inner_start"] = cessation_range.inner.start
-		to_assign["date:cessation_inner_end"] = cessation_range.inner.end
-		to_assign["date:cessation_outer_start"] = cessation_range.outer.start
-		to_assign["date:cessation_outer_end"] = cessation_range.outer.end
+		to_assign["date:cessation_inner_start"] = cessation_range.Inner.Start
+		to_assign["date:cessation_inner_end"] = cessation_range.Inner.End
+		to_assign["date:cessation_outer_start"] = cessation_range.Outer.Start
+		to_assign["date:cessation_outer_end"] = cessation_range.Outer.End
 	}
 
 	for k, v := range to_assign {
@@ -78,7 +67,7 @@ func AppendEDTFRanges(ctx context.Context, body []byte) ([]byte, error) {
 	return body, nil
 }
 
-func deriveRanges(props gjson.Result, path string) (*date_range, error) {
+func deriveRanges(props gjson.Result, path string) (*unix.DateRange, error) {
 
 	edtf_rsp := props.Get(path)
 
@@ -88,131 +77,15 @@ func deriveRanges(props gjson.Result, path string) (*date_range, error) {
 
 	edtf_str := edtf_rsp.String()
 
-	if !isValid(edtf_str) {
-		return nil, nil
-	}
-
-	edtf_dt, err := parser.ParseString(edtf_str)
+	derived, date_range, err := unix.DeriveRanges(edtf_str)
 
 	if err != nil {
-		return nil, fmt.Errorf("Failed to parse '%s', %w", path, err)
+		return nil, err
 	}
 
-	start := edtf_dt.Start
-	end := edtf_dt.End
-
-	start_lower := start.Lower
-	start_upper := start.Upper
-
-	end_lower := end.Lower
-	end_upper := end.Upper
-
-	if start_lower == nil {
+	if !derived {
 		return nil, nil
 	}
 
-	if start_upper == nil {
-		return nil, nil
-	}
-
-	if end_lower == nil {
-		return nil, nil
-	}
-
-	if end_upper == nil {
-		return nil, nil
-	}
-
-	start_lower_ts := start_lower.Timestamp
-	start_upper_ts := start_upper.Timestamp
-
-	end_lower_ts := end_lower.Timestamp
-	end_upper_ts := end_upper.Timestamp
-
-	if start_lower_ts == nil {
-		return nil, nil
-	}
-
-	if start_upper_ts == nil {
-		return nil, nil
-	}
-
-	if end_lower_ts == nil {
-		return nil, nil
-	}
-
-	if end_upper_ts == nil {
-		return nil, nil
-	}
-
-	outer_start := start_lower_ts.Unix()
-	outer_end := end_upper_ts.Unix()
-
-	inner_start := start_upper_ts.Unix()
-	inner_end := end_lower_ts.Unix()
-
-	outer := &date_span{
-		start: outer_start,
-		end:   outer_end,
-	}
-
-	inner := &date_span{
-		start: inner_start,
-		end:   inner_end,
-	}
-
-	r := &date_range{
-		outer: outer,
-		inner: inner,
-	}
-
-	return r, nil
-}
-
-func isValid(edtf_str string) bool {
-
-	if isOpen(edtf_str) {
-		return false
-	}
-
-	if isUnknown(edtf_str) {
-		return false
-	}
-
-	if isUnspecified(edtf_str) {
-		return false
-	}
-
-	return true
-}
-
-func isOpen(edtf_str string) bool {
-
-	switch edtf_str {
-	case edtf.OPEN, edtf.OPEN_2012:
-		return true
-	default:
-		return false
-	}
-}
-
-func isUnknown(edtf_str string) bool {
-
-	switch edtf_str {
-	case edtf.UNKNOWN, edtf.UNKNOWN_2012:
-		return true
-	default:
-		return false
-	}
-}
-
-func isUnspecified(edtf_str string) bool {
-
-	switch edtf_str {
-	case edtf.UNSPECIFIED, edtf.UNSPECIFIED_2012:
-		return true
-	default:
-		return false
-	}
-
+	return date_range, nil
 }
--- go.mod
+++ go.mod
@@ -8,15 +8,16 @@ go 1.16
 // Good times...
 
 require (
-	github.com/cenkalti/backoff/v4 v4.1.2
+	github.com/cenkalti/backoff/v4 v4.1.3
 	github.com/elastic/go-elasticsearch/v7 v7.13.0
-	github.com/sfomuseum/go-edtf v0.3.1
+	github.com/fortytw2/leaktest v1.3.0 // indirect
+	github.com/sfomuseum/go-edtf v1.1.1
 	github.com/sfomuseum/go-flags v0.8.2
-	github.com/tidwall/gjson v1.14.0
+	github.com/tidwall/gjson v1.14.1
 	github.com/tidwall/sjson v1.2.4
-	github.com/whosonfirst/go-whosonfirst-edtf v0.3.0
+	github.com/whosonfirst/go-whosonfirst-edtf v0.3.1
 	github.com/whosonfirst/go-whosonfirst-iterate-git/v2 v2.1.0
-	github.com/whosonfirst/go-whosonfirst-iterate/v2 v2.0.1
+	github.com/whosonfirst/go-whosonfirst-iterate/v2 v2.0.2
 	github.com/whosonfirst/go-whosonfirst-placetypes v0.3.0
 	github.com/whosonfirst/go-whosonfirst-uri v1.2.0
 	gopkg.in/olivere/elastic.v3 v3.0.75
